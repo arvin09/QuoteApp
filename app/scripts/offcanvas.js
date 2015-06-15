73e57a6bb50f70a23e513107bde05338b31fc27f
arvin09 <awaghma@Arvin-2.local>
2015-06-15 23:05:54 +0530

    menu
    Making the menu slide the entire content with the menus

--- app/scripts/offcanvas.js
+++ app/scripts/offcanvas.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  $('.quote-container').css({height: window.screen.height + "px"})	
   $('[data-toggle="offcanvas"]').click(function () {
     $('.row-offcanvas').toggleClass('active')
   });
