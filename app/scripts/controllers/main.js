5dc8e69d412477c013d49882550825a36cd8972b
arvin09 <awaghma@Arvin-2.local>
2015-06-18 20:54:29 +0530

    Adding Filter Logic
    Filtering quotes according to types

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,20 +7,21 @@
  * # MainCtrl
  * Controller of the quoteAppApp
  */
-angular.module('quoteAppApp')
-  .controller('MainCtrl', function ($scope) {
+var app = angular.module('quoteApp');
+ app.controller('MainCtrl', ['$scope','$routeParams',function($scope,$routeParams){
+    $scope.quoteType = $routeParams.type || '';
     $scope.quotes = [
-      {id:1,desc:'Walking with a friend in the dark is better than walking alone in the light.',type:'M'},
+      {id:1,desc:'Walking with a friend in the dark is better than walking alone in the light.',type:'motivation'},
       {id:2,desc:'The greatest gift of life is friendship, and I have received it.'},
-      {id:3,desc:'I cannot even imagine where I would be today were it not for that handful of friends who have given me a heart full of joy. Lets face it, friends make life a lot more fun.',type:'M'},
-      {id:1,desc:'Walking with a friend in the dark is better than walking alone in the light.',type:'M'},
-      {id:2,desc:'The greatest gift of life is friendship, and I have received it.'},
-      {id:3,desc:'I cannot even imagine where I would be today were it not for that handful of friends who have given me a heart full of joy. Lets face it, friends make life a lot more fun.',type:'M'},
-      {id:1,desc:'Walking with a friend in the dark is better than walking alone in the light.',type:'M'},
-      {id:2,desc:'The greatest gift of life is friendship, and I have received it.',type:'M'},
-      {id:3,desc:'I cannot even imagine where I would be today were it not for that handful of friends who have given me a heart full of joy. Lets face it, friends make life a lot more fun.',type:'M'},
-      {id:1,desc:'Walking with a friend in the dark is better than walking alone in the light.',type:'M'},
-      {id:2,desc:'The greatest gift of life is friendship, and I have received it.',type:'M'},
-      {id:3,desc:'I cannot even imagine where I would be today were it not for that handful of friends who have given me a heart full of joy. Lets face it, friends make life a lot more fun.',type:'M'}
+      {id:3,desc:'I cannot even imagine where I would be today were it not for that handful of friends who have given me a heart full of joy. Lets face it, friends make life a lot more fun.',type:'funny'},
+      {id:1,desc:'Walking with a friend in the dark is better than walking alone in the light.',type:'friendship'},
+      {id:2,desc:'The greatest gift of life is friendship, and I have received it.',type:'life'},
+      {id:3,desc:'I cannot even imagine where I would be today were it not for that handful of friends who have given me a heart full of joy. Lets face it, friends make life a lot more fun.',type:'life'},
+      {id:1,desc:'Walking with a friend in the dark is better than walking alone in the light.',type:'love'},
+      {id:2,desc:'The greatest gift of life is friendship, and I have received it.',type:'motivation'},
+      {id:3,desc:'I cannot even imagine where I would be today were it not for that handful of friends who have given me a heart full of joy. Lets face it, friends make life a lot more fun.',type:'friendship'},
+      {id:1,desc:'Walking with a friend in the dark is better than walking alone in the light.',type:'birthday'},
+      {id:2,desc:'The greatest gift of life is friendship, and I have received it.',type:'inspiration'},
+      {id:3,desc:'I cannot even imagine where I would be today were it not for that handful of friends who have given me a heart full of joy. Lets face it, friends make life a lot more fun.',type:'motivation'}
     ];
-  });
+  }]);
