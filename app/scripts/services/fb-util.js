45f8b5a8de6cde6ef4ed64c67c93e1b7ec3f04eb
Arvin <arvin09@gmail.com>
2015-06-23 02:15:51 +0530

    Adding service for Facebook Authentication
    Adding service for Facebook Authentication

---
+++ app/scripts/services/fb-util.js
@@ -0,0 +1,43 @@
+'use strict';
+var app = angular.module('fbAuth',[]);
+app.factory('srvAuth', ['$rootScope',function($rootScope) {
+      var srvAuth = {};
+      srvAuth.fblogin = function() {
+        FB.login(function (response) {
+          if (response.status === 'connected') {
+            // You can now do what you want with the data fb gave you.
+            console.info(response);
+          }
+        });        
+      };
+
+      srvAuth.watchLoginChange = function() {
+        var _self = this;
+        FB.Event.subscribe('auth.authResponseChange', function(res) {
+          if (res.status === 'connected') {
+            FB.api('/me', function(res) {
+              $rootScope.$apply(function() {
+                $rootScope.user = _self.user = res;
+                $rootScope.user.login = _self.user.login = true;
+                $rootScope.user.profilePic = _self.user.profilePic = 'http://graph.facebook.com/'+res.id+'/picture'; 
+                console.info($rootScope.user);
+              });
+            });
+          } else {
+            console.log('Not Connected');
+          }
+        });
+      };
+
+      srvAuth.logout = function() {
+        var _self = this;
+        FB.logout(function(response) {
+          $rootScope.$apply(function() {
+            $rootScope.user = _self.user = {};
+          });
+          console.info(response);
+        });
+      };
+
+      return srvAuth;
+}]);
