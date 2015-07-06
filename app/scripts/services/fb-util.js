0de39fa5d85cbef64903ea4153a0872f56bee955
arvin09 <arvin09@gmail.com>
2015-07-06 10:27:17 +0530

    Updated FB Util Code to be more modularise
    Updated FB Util Code to be more modularise & align code for gplus util

--- app/scripts/services/fb-util.js
+++ app/scripts/services/fb-util.js
@@ -1,46 +1,46 @@
 'use strict';
 var app = angular.module('fbAuth',[]);
 app.factory('srvAuth', ['$rootScope',function($rootScope) {
-      var srvAuth = {};
-      srvAuth.fblogin = function() {
-        FB.login();        
-      };
+  var srvAuth = {};
+  srvAuth.fblogin = function() {
+    FB.login();        
+  };
 
-      srvAuth.watchLoginChange = function() {
-        FB.getLoginStatus(function(response) {
-          srvAuth.isLoggedIn(response);
+  srvAuth.watchLoginChange = function() {
+    FB.getLoginStatus(function(response) {
+      srvAuth.isLoggedIn(response);
+    });
+
+    FB.Event.subscribe('auth.statusChange', function(response){
+     srvAuth.isLoggedIn(response);
+   });
+  };
+
+  srvAuth.isLoggedIn = function(response){
+    var _self = this;
+    if (response.status === 'connected') {
+      FB.api('/me', function(response) {
+        $rootScope.$apply(function() {
+          $rootScope.socialApi = 'facebook';
+          $rootScope.user = _self.user = response;
+          $rootScope.user.login = _self.user.login = true;
+          $rootScope.user.profilePic = _self.user.profilePic = 'http://graph.facebook.com/'+response.id+'/picture';
         });
+      });
+    }else{
+      console.log('Not Connected');
+    }  
+  };
 
-        FB.Event.subscribe('auth.statusChange', function(response){
-           srvAuth.isLoggedIn(response);
-        });
-      };
+  srvAuth.logout = function() {
+    var _self = this;
+    FB.logout(function(response) {
+      $rootScope.$apply(function() {
+        $rootScope.user = _self.user = {};
+      });
+      console.info(response);
+    });
+  };
 
-      srvAuth.isLoggedIn = function(response){
-        var _self = this;
-         if (response.status === 'connected') {
-            FB.api('/me', function(response) {
-              $rootScope.$apply(function() {
-                $rootScope.socialApi = 'facebook';
-                $rootScope.user = _self.user = response;
-                $rootScope.user.login = _self.user.login = true;
-                $rootScope.user.profilePic = _self.user.profilePic = 'http://graph.facebook.com/'+response.id+'/picture';
-              });
-            });
-          }else{
-            console.log("Not Connected");
-          }  
-      };
-
-      srvAuth.logout = function() {
-        var _self = this;
-        FB.logout(function(response) {
-          $rootScope.$apply(function() {
-            $rootScope.user = _self.user = {};
-          });
-          console.info(response);
-        });
-      };
-
-      return srvAuth;
+  return srvAuth;
 }]);
