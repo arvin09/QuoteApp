6cc0dec853aa167fd47d70d801af26e139b124c8
Arvind Waghmare <Arvind.Waghmare@morningstar.com>
2015-09-03 23:02:30 +0530

    Adding set favourites

--- app/scripts/directives/set-favourite.js
+++ app/scripts/directives/set-favourite.js
@@ -5,6 +5,7 @@ app.directive('setFavourite', function(){
   return {
     scope: {
       isFav: '=',
+      id :'@',
       on: '@',
       off: '@'
     },
@@ -12,8 +13,22 @@ app.directive('setFavourite', function(){
     replace: true,
     template: '<span class="glyphicon {{isFav ? on : off}}" aria-hidden="true" ng-click="toggle()"></span>',
     link: function link(scope){	
+
 	    scope.toggle = function toggle(){
 	      scope.isFav = !scope.isFav;
+
+        var favourite = [];
+
+        if(localStorage.myfavourite){
+          favourite = JSON.parse(localStorage.myfavourite);
+        }else{
+           localStorage.setItem('myfavourite',favourite);
+        }
+        
+        scope.isFav ? favourite.push(scope.id) : favourite.splice(favourite.indexOf(scope.id),1);  
+
+        localStorage.myfavourite = JSON.stringify(favourite);
+
 	    };
   	}
   };
